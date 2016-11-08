937c487297389c0ad518bb203eee1db14de65ef0
Philipp Morozov <indiekid.philipp@gmail.com>
2016-11-08 17:05:26 +0300

    Now 2 leds fading in and out

---
+++ STM32L1/PWM Leds/project_files/main.c
@@ -0,0 +1,84 @@
+#include "stm32l1xx.h"
+
+//http://visualgdb.com/tutorials/arm/stm32/pwm/
+
+void InitializeLEDs()
+{
+		GPIO_InitTypeDef gpioStructure;
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+
+    gpioStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+    gpioStructure.GPIO_Mode = GPIO_Mode_AF;
+    gpioStructure.GPIO_Speed = GPIO_Speed_40MHz;
+    GPIO_Init(GPIOB, &gpioStructure);
+}
+
+void InitializeTimer(int period)
+{
+		TIM_TimeBaseInitTypeDef timerInitStructure;
+		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+    timerInitStructure.TIM_Prescaler = 0;
+    timerInitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    timerInitStructure.TIM_Period = period;
+		//timerInitStructure.TIM_RepetitionCounter = 0;
+    timerInitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInit(TIM4, &timerInitStructure);
+    TIM_Cmd(TIM4, ENABLE);
+}
+
+void InitializePWMChannel()
+{
+    TIM_OCInitTypeDef outputChannelInit = {0,};
+    outputChannelInit.TIM_OCMode = TIM_OCMode_PWM1;
+    outputChannelInit.TIM_Pulse = 1000;
+    outputChannelInit.TIM_OutputState = TIM_OutputState_Enable;
+    outputChannelInit.TIM_OCPolarity = TIM_OCPolarity_High;
+ 
+    TIM_OC1Init(TIM4, &outputChannelInit);
+    TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+ 
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_TIM4);
+}
+
+void InitializePWMChannel2()
+{
+    TIM_OCInitTypeDef outputChannelInit = {0,};
+    outputChannelInit.TIM_OCMode = TIM_OCMode_PWM1;
+    outputChannelInit.TIM_Pulse = 1000;
+    outputChannelInit.TIM_OutputState = TIM_OutputState_Enable;
+    outputChannelInit.TIM_OCPolarity = TIM_OCPolarity_High;
+ 
+    TIM_OC2Init(TIM4, &outputChannelInit);
+    TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
+ 
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_TIM4);
+}
+
+ 
+
+
+int main()
+{
+	int i,j;
+	InitializeLEDs();
+	InitializeTimer(500);
+	InitializePWMChannel();
+	InitializePWMChannel2();
+	
+	for (;;)
+    {
+			for (i=0;i<=800;i++) //brightness 0..1000
+			{
+				TIM4->CCR1 = i;
+				TIM4->CCR2 = 800 - i;
+				for(j=0;j<1000;j++);
+			}
+			for (i=0;i<=800;i++) //brightness 0..1000
+			{
+				TIM4->CCR1 = 800 - i;
+				TIM4->CCR2 = i;
+				for(j=0;j<1000;j++);
+			}
+    }
+}
