95093da8568fd6532c5d37dee03175f01b99d87a
Philipp Morozov <indiekid.philipp@gmail.com>
2016-11-08 13:20:42 +0300

    Blinking led

--- stml32l1-skeleton/project_files/main.c
+++ stml32l1-skeleton/project_files/main.c
@@ -1,64 +1,40 @@
 #include "stm32l1xx.h"
 
-uint8_t i = 0;
-
-void TIM6_IRQHandler(void)
+void TIM2_IRQHandler(void)
 {
-		TIM6->SR &= ~TIM_SR_UIF;
-		if (i == 0) GPIOB->BSRRL |= GPIO_BSRR_BS_6;
-		else GPIOB->BSRRH |= GPIO_BSRR_BS_6;
-		i = ~i;
+	TIM2->SR &= ~TIM_SR_UIF;
+	GPIOB->ODR ^= GPIO_OTYPER_ODR_6;
 }
- 
-int main(void)
+
+int main()
 {
-   GPIO_InitTypeDef port;
-    
-   /* RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	GPIO_InitTypeDef port;
 	
+	NVIC_SetPriority(TIM2_IRQn, 1);
+  NVIC_EnableIRQ(TIM2_IRQn);
 	
-    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
-		RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);*/
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
 	
-		RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+	port.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
+  port.GPIO_Mode = GPIO_Mode_OUT;
+  port.GPIO_OType = GPIO_OType_PP;
+  port.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  port.GPIO_Speed = GPIO_Speed_2MHz;
 	
-		NVIC_SetPriority(TIM6_IRQn, 1); 
-		NVIC_EnableIRQ(TIM6_IRQn);
+  GPIO_Init(GPIOB, &port);
 	
-		RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
-		GPIOB->MODER |= GPIO_MODER_MODER6_0;
-		GPIOB->OTYPER &= ~GPIO_OTYPER_OT_6;
-		GPIOB->PUPDR &= ~GPIO_PUPDR_PUPDR6;
-		GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR6;
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); //RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 	
-		RCC->APB1ENR |= RCC_APB1ENR_TIM6EN; 
-		TIM6->PSC = 0xFFFF; 
-		TIM6->ARR = 0x000F;
-		TIM6->DIER |= TIM_DIER_UIE; 
-		TIM6->CR1 |= TIM_CR1_CEN;
+	TIM2->PSC = 2096;
+  TIM2->ARR = 500;
+  TIM2->DIER |= TIM_DIER_UIE;
+  TIM2->CR1 |= TIM_CR1_CEN;
+	
+	GPIO_SetBits(GPIOB, GPIO_Pin_6);
+	GPIO_SetBits(GPIOB, GPIO_Pin_7);
+	
+	while (1)
+	{
 		
-	
-	
-    port.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-    port.GPIO_Mode = GPIO_Mode_OUT;
-    port.GPIO_OType = GPIO_OType_PP;
-    port.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    port.GPIO_Speed = GPIO_Speed_2MHz;
-    
-    GPIO_Init(GPIOB, &port);
-		/*/////////////////////////////////////
-    port.GPIO_Pin = GPIO_Pin_0;
-    port.GPIO_Mode = GPIO_Mode_IN;
-    port.GPIO_OType = GPIO_OType_PP;
-    port.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    port.GPIO_Speed = GPIO_Speed_2MHz;
-    
-    GPIO_Init(GPIOA, &port);
-		*/
-		
-    
-    while(1)
-    {
-        GPIO_SetBits(GPIOB, GPIO_Pin_7);
-    }
+	}
 }
